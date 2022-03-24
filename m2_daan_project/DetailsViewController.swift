00b36f77c788c7a67a6f0d9a7b71348e33f96de0
Ladislas WALCAK <walcak.ladislas@gmail.com>
2022-03-24 21:01:04 +0100

    [ADD] Can now add new timers

--- m2_daan_project/DetailsViewController.swift
+++ m2_daan_project/DetailsViewController.swift
@@ -15,6 +15,48 @@ class DetailsViewController: UIViewController, UITableViewDataSource {
         self._title.text = self.category.name
     }
     
+    @IBAction func addTimer() {
+        let alert = UIAlertController(title: "Ajouter un timer", message: "Veuillez entrer le nom du timer que vous voulez ajouter", preferredStyle: .alert)
+        var textField = UITextField()
+        let action = UIAlertAction(title: "Ajouter", style: . default) { (action) in
+            // Code qui dit ce qui se passe qd on clique sur le bouton Add
+            if textField.text != .some("") && textField.text != .none {
+                let newTimer = Timer(context: self._context)
+                newTimer.name = textField.text
+                newTimer.category = self.category
+            
+                do {
+                    try self._context.save()
+                    print("Saved timer: '" + newTimer.name! + "'")
+                } catch {
+                    print("Can't save: \(error)")
+                }
+            } else {
+                let errorAlert = UIAlertController(title: "Erreur de sauvegarde", message: "Il faut renseigner un nom pour le nouveau timer", preferredStyle: .alert)
+                self.present(errorAlert, animated: true, completion: {
+                    errorAlert.view.superview?.isUserInteractionEnabled = true
+                    errorAlert.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.dismissOnTapOutside)))
+                })
+            }
+            
+            self._tableview.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Nom"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // Method to close the alert
+    @objc func dismissOnTapOutside() {
+       self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.category.timers?.count ?? 0
     }
@@ -34,6 +76,7 @@ class DetailsViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             let timer = self.category.timers![indexPath.row] as! Timer
+            timer.category = nil
             
             print("Timer '\(timer.name!)' supprimée !")
             
@@ -41,6 +84,4 @@ class DetailsViewController: UIViewController, UITableViewDataSource {
             self._tableview.deleteRows(at: [indexPath], with: .fade)
         }
     }
-    
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
 }
